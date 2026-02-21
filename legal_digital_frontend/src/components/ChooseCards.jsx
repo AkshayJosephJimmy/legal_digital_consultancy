18c28d2053bdf433b99b8ead06ff7a8774b82dcc
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2026-02-21 16:47:06 +0530

    changed choose cards to neutral

--- legal_digital_frontend/src/components/ChooseCards.jsx
+++ legal_digital_frontend/src/components/ChooseCards.jsx
@@ -11,7 +11,7 @@ function ChooseCards({feature}) {
 
 
 return(
-    <div className="h-70 w-50 bg-white rounded-4xl hover:translate-y-2 transition-all duration-300 ">
+    <div className="h-70 w-50 bg-neutral-100 rounded-4xl hover:translate-y-2 transition-all duration-300 ">
         <h3 className="text-2xl font-bold text-center p-4">{feature.title}</h3>
         
         <p className="text-center p-3">{feature.description}</p>
