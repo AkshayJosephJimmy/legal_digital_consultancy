31cf7aaab4c36a85b932d4af9fc26deebdd8843f
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2026-02-21 19:35:09 +0530

    made team section responsive

--- legal_digital_frontend/src/components/Team.jsx
+++ legal_digital_frontend/src/components/Team.jsx
@@ -9,25 +9,24 @@ const image = "/team.svg";
 
     return(
         <div className="w-full h-full mt-25 bg-neutral-300">
-            <div className="p-15 w-full  bg-neutral-800 text-center ">
+            <div className="p-10 lg:p-15 w-full  bg-neutral-800 text-center ">
 
             <h1 className="text-2xl lg:text-4xl font-bold text-center text-neutral-200">Our Team</h1>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-col lg:flex-row">
 
              <div className=" ">
-                <div className="px-20 py-10 w-full h-fit bg-neutral-300 text-start align-middle ">
+                <div className="px-10 lg:px-20 py-10 w-full h-fit bg-neutral-300 text-start align-middle ">
 
-            <p className="text-gray-600 text-2xl font-semibold">Our Team Consists of  <span className="text-neutral-800 text-6xl block"> <span>Legal Professionals</span>, <span className="text-neutral-800 text-6xl block">Digital Specialists</span><span className="text-neutral-800 text-6xl block">and Developers</span></span></p>
+            <p className="text-gray-600 text-2xl font-semibold">Our Team Consists of  <span className="text-neutral-800 text-4xl lg:text-6xl block"> <span>Legal Professionals</span>, <span className="text-neutral-800 text-4xl lg:text-6xl block">Digital Specialists</span><span className="text-neutral-800 text-4xl lg:text-6xl block">and Developers</span></span></p>
                 </div>
-                <div className="px-20 py-10 w-full h-fit bg-neutral-300 text-start align-middle ">
+                <div className="px-10 lg:px-20 py-10 w-full h-fit bg-neutral-300 text-start align-middle ">
 
-
-            <p className="text-gray-600 text-2xl font-semibold">we work collaboratively to deliver <span className="text-neutral-800 text-6xl block"> <span>Ethically Compliant</span>, <span className="text-neutral-800 text-6xl block">Digital Services </span></span> for law firms and lawyers.</p>
+            <p className="text-gray-600 text-2xl font-semibold">we work collaboratively to deliver <span className="text-neutral-800 text-4xl lg:text-6xl block"> <span>Ethically Compliant</span>, <span className="text-neutral-800 text-4xl lg:text-6xl block">Digital Services </span></span> for law firms and lawyers.</p>
                 </div>
          </div>
 
-            <figure className="px-10 w-200 top-70 object-cover mt-20">
+            <figure className="lg:px-10 w-110 lg:w-200 top-70 object-cover mt-20">
                 <img src={image} alt="Our Team" className="object-cover "/>
             </figure>
             </div>
