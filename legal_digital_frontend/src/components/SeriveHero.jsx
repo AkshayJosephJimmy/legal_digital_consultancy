577883d082b1645a278901a42b917cbdead8b596
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2026-02-21 13:33:10 +0530

    made section hero optimzation

--- legal_digital_frontend/src/components/SeriveHero.jsx
+++ legal_digital_frontend/src/components/SeriveHero.jsx
@@ -8,8 +8,8 @@ function SeriveHero() {
             <>
                 <div className=" relative mt-20">
     
-                <img src={image} alt="Services Image" className="w-full h-100 object-cover"/>
-    
+                <img src="https://ik.imagekit.io/ylq75x7e0//serviceHero.jpg?tr=f-auto,q-100,w-1600" alt="Services Image" className="w-full h-100 object-cover"/>
+                            
                 <h1 className="absolute text-6xl top-60 left-20 font-extrabold text-neutral-300">SERVICES</h1>
                 </div>
                 
